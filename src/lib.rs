168dd4dc39a712252a0efbd30055c78977611898
Bammez <Kingnuel4@gmail.com>
2024-05-04 11:00:26 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
-/// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {isbn:?} a valid ISBN number?");
+    let mut sum = 0;
+    let mut multiplier = 10;
+
+    for c in isbn.chars().filter(|&c| c.is_ascii_digit() || c == 'X') {
+        if let Some(digit) = c.to_digit(10) {
+            sum += multiplier * digit;
+            multiplier -= 1;
+        } else if c == 'X' && multiplier == 1 {
+            sum += 10;
+            multiplier -= 1;
+        } else {
+            return false;
+        }
+    }
+
+    sum % 11 == 0 && multiplier == 0
 }
+
+
+
+
